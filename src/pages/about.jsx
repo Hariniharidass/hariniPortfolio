67933ca604b22dc1805ae84996e119df94c7aacf
Harini <nharinirmd@gmail.com>
2025-04-26 23:38:01 +0200

    updated about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -2,8 +2,38 @@ import React from "react";
 
 function about() {
   return (
-    <div>
-      <button>About</button>
+    <div className="flex flex-col justify-center items-center mx-20 my-20 text-2xl">
+      <p className=" scale-z-200 my-5 text-3xl">
+        A dedicated Frontend Developer resuming my career journey with renewed
+        energy and a deep passion for creating exceptional web experiences.
+      </p>
+      <ol>
+        <li className="list-disc my-3 text-2xl">
+          Practical experience developing a full-stack web application for
+          auction services.
+        </li>
+        <li className="list-disc my-3 text-2xl">
+          Master's thesis at Axis Communications focused on web performance
+          optimization.
+        </li>
+        <li className="list-disc my-3 text-2xl">
+          Actively expanding skills in modern frontend technologies like React .
+        </li>
+        <li className="list-disc my-3 text-2xl">
+          Maintained technical edge through continuous learning during a career
+          break.
+        </li>
+        <li className="list-disc my-3 text-2xl">
+          Adaptable, resilient, and possessing strong time management skills.
+        </li>
+        <li className="list-disc my-3 text-2xl">
+          Proven technical skills in HTML, CSS, JavaScript, NodeJS, and RESTful
+          APIs.
+        </li>
+        <li className="list-disc my-3 text-2xl">
+          Driven to contribute effectively to development teams.
+        </li>
+      </ol>
     </div>
   );
 }
