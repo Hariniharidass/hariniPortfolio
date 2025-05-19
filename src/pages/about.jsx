c4a297be682f074bcf00e03f017183e70e5cdfa0
Harini <nharinirmd@gmail.com>
2025-05-19 13:05:32 +0200

    modified about me page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,4 +1,4 @@
-import persononpeak from "../../public/images/persononpeak.jpg";
+import developer from "../../public/images/developer.png";
 import workingwoman from "../../public/images/workingwoman.jpg";
 import performance from "../../public/images/performance.jpg";
 import react from "../../public/images/reacttech.jpg";
@@ -12,25 +12,25 @@ function about() {
       <h1>Get to know me...</h1>
       <div className="grid gap-5">
         <div className="grid grid-cols-3 m-4 p-2  bg-gray-300 ">
-          <div className="self-center col-span-3 bg-[url('/images/persononpeak.jpg')] h-120 w-full bg-cover bg-center">
-            <p className=" md:my-5 md:text-3xl text-xl text-center text-white">
+          <div className="self-center col-span-2 w-full ">
+            <p className="col-span-2 md:my-5 md:text-3xl text-xl text-center">
               I'm a dedicated Frontend Developer resuming my career journey with
               renewed energy and a deep passion for creating exceptional web
               experiences.
             </p>
           </div>
-          {/*  <div className="justify-self-center">
+          <div className="justify-self-center">
             <img
-              className="md:w-70 md:h-70 w-30 h-30"
-              src={persononpeak}
-              alt="a person climbing on a peak"
+              className="md:w-70 md:h-70 w-30 h-30 rounded-full"
+              src={developer}
+              alt="female developer"
             />
-          </div> */}
+          </div>
         </div>
         <div className="grid grid-cols-3 m-4 p-2">
           <div className="justify-self-center">
             <img
-              className="md:w-70 md:h-70 w-30 h-30"
+              className="md:w-70 md:h-70 w-30 h-30 rounded-full"
               src={workingwoman}
               alt="typing on a laptop"
             />
@@ -38,96 +38,98 @@ function about() {
           <div className="self-center col-span-2">
             <p className=" md:my-5 md:text-3xl text-base text-center ">
               Practical experience in developing full-stack web applications.
+              Worked at DASC as Frontend developer.
             </p>
           </div>
         </div>
         <div className="grid grid-cols-3 m-4 p-2  bg-gray-300">
           <div className="self-center col-span-2">
             <p className=" md:my-5 md:text-3xl text-base text-center ">
-              Master's thesis at Axis Communications focused on web performance
-              optimization.
+              Master in Computer Science from Malmö University. Master's thesis
+              at Axis Communications focused on web performance optimization.
             </p>
           </div>
           <div className="justify-self-center">
             <img
-              className="md:w-70 md:h-70 w-30 h-30"
+              className="md:w-70 md:h-70 w-30 h-30 rounded-full"
               src={performance}
-              alt="a person climbing on a peak"
+              alt="graph showing performance"
             />
           </div>
         </div>
         <div className="grid grid-cols-3 m-4 p-2 ">
           <div className="justify-self-center">
             <img
-              className="md:w-70 md:h-70 w-30 h-30"
+              className="md:w-70 md:h-70 w-30 h-30 rounded-full"
               src={react}
-              alt="a person climbing on a peak"
+              alt="React tech"
             />
           </div>
           <div className="self-center col-span-2">
             <p className=" md:my-5 md:text-3xl text-base text-center ">
-              Actively expanding skills in modern frontend technologies like
-              React.
+              IT Frontend Developement Course from Sundsgården Folkhögskola,
+              Helsingborg. Actively refining and expanding skills in modern
+              frontend technologies like React.
             </p>
           </div>
         </div>
-
         <div className="grid grid-cols-3 m-4 p-2  bg-gray-300">
           <div className="self-center col-span-2">
             <p className=" md:my-5 md:text-3xl text-base text-center ">
-              Maintained technical edge through continuous learning during a
-              career break.
+              Driven to contribute effectively and collaborate within teams.
             </p>
           </div>
           <div className="justify-self-center">
             <img
-              className="md:w-70 md:h-70 w-30 h-30"
-              src={continuouslearning}
-              alt="a person climbing on a peak"
+              className="md:w-70 md:h-70 w-30 h-30 rounded-full"
+              src={teamwork}
+              alt="two person as a team"
             />
           </div>
         </div>
-        <div className="grid grid-cols-3 m-4 p-2">
+        <div className="grid grid-cols-3 m-4 p-2  ">
           <div className="justify-self-center">
             <img
-              className="md:w-70 md:h-70 w-30 h-30"
-              src={resilient}
-              alt="a person climbing on a peak"
+              className="md:w-70 md:h-70 w-30 h-30 rounded-full"
+              src={continuouslearning}
+              alt="developers workspace"
             />
           </div>
+          <div className="self-center col-span-2">
+            <p className=" md:my-5 md:text-3xl text-base text-center ">
+              Maintained technical edge through continuous learning during a
+              career break. Tried my hands on building simple React Native
+              application.
+            </p>
+          </div>
+        </div>
+        <div className="grid grid-cols-3 m-4 p-2  bg-gray-300">
           <div className="self-center col-span-2">
             <p className=" md:my-5 md:text-3xl text-base text-center ">
               Adaptable, resilient, and possessing strong time management
               skills.
             </p>
           </div>
-        </div>
-        <div className="grid grid-cols-3 m-4 p-2  bg-gray-300">
-          <div className="self-center col-span-2">
-            <p className=" md:my-5 md:text-3xl text-base text-center ">
-              Proven technical skills in HTML, CSS, JavaScript, NodeJS, and
-              RESTful APIs.
-            </p>
-          </div>
           <div className="justify-self-center">
             <img
-              className="md:w-70 md:h-70 w-30 h-30"
-              src={techskills}
-              alt="a person climbing on a peak"
+              className="md:w-70 md:h-70 w-30 h-30 rounded-full"
+              src={resilient}
+              alt="time management"
             />
           </div>
         </div>
         <div className="grid grid-cols-3 m-4 p-2 ">
           <div className="justify-self-center">
             <img
-              className="md:w-70 md:h-70 w-30 h-30"
-              src={teamwork}
-              alt="a person climbing on a peak"
+              className="md:w-70 md:h-70 w-30 h-30 rounded-full"
+              src={techskills}
+              alt="code on screen"
             />
           </div>
           <div className="self-center col-span-2">
             <p className=" md:my-5 md:text-3xl text-base text-center ">
-              Driven to contribute effectively and collaborate within teams.
+              Proven technical skills in HTML, CSS, JavaScript, NodeJS, and
+              RESTful APIs, responsive design.
             </p>
           </div>
         </div>
